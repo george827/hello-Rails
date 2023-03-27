e16a7842eb9993d85b8087fec2b43a62ba1b1dcf
george827 <kinyanjuigeorge827@gmail.com>
2023-03-27 21:05:18 +0300

    hello rails

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'pages#hello'
+  get 'pages/hello'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
